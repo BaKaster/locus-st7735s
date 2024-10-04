fc334701062cde9578c23c34ea4c8c6356e421f4
Ustin <60433149+BaKaster@users.noreply.github.com>
2024-10-04 15:06:49 +0300

    Added page title struct

--- Core/Inc/App/Lcd.hpp
+++ Core/Inc/App/Lcd.hpp
@@ -1,128 +1,112 @@
-//
-// Lcd.hpp
-//
-// Created on: Feb 07, 2022
-//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
-//
-
 #ifndef CORE_INC_APP_LCD_HPP
 #define CORE_INC_APP_LCD_HPP
 
 #include "KvGrid.hpp"
 
 namespace Lcs {
-
 class Info;
-
 }  // namespace Lcs
 
 namespace Graphics {
-
 namespace Wid {
-
 template <unsigned short>
 class Slider;
 
 class Text;
 class FillDisplay;
-
 }  // namespace Wid
 
 namespace Prio {
-
 class OpQueue;
-
 }  // namespace Prio
 
 class Color;
 class Area;
 class Offset;
-
 }  // namespace Graphics
 
 namespace App {
 
 class Lcd {
 private:
-	enum class Page {
-		Main = 0,
-		Active,
-		Enabled,
-		Amplitude,
+    enum class Page {
+        Main = 0,
+        Active,
+        Enabled,
+        Amplitude,
 
-		Max,
-	} page;
+        Max,
+    } page;
 
-	static constexpr short knPages = static_cast<short>(Page::Max);
+    static constexpr short knPages = static_cast<short>(Page::Max);
 
-	struct {
-		Graphics::Prio::OpQueue background;
-		Graphics::Prio::OpQueue kvGrid;
-		struct {
-			bool background = false;
-			bool kvGrid = false;
-		} update;
-	} ren;
+    struct {
+        Graphics::Prio::OpQueue background;
+        Graphics::Prio::OpQueue kvGrid;
+        struct {
+            bool background = false;
+            bool kvGrid = false;
+        } update;
+    } ren;
 
-	Graphics::BgColor backgroundColor;
-	Graphics::Wid::FillDisplay fillDisplay;
-	Graphics::Wid::Slider<knPages> slider;
+    Graphics::BgColor backgroundColor;
+    Graphics::Wid::FillDisplay fillDisplay;
+    Graphics::Wid::Slider<knPages> slider;
 
-	Graphics::Color textColor;
-	Graphics::Area textSize;  // Text size
-	Graphics::Offset gridOffset;
+    Graphics::Color textColor;
+    Graphics::Area textSize;  // Text size
+    Graphics::Offset gridOffset;
 
-	union {
-		KvGrid<3> *kvGrid3;
-		KvGrid<4> *kvGrid4;
-		KvGrid<6> *kvGrid6;
-	};
+    union {
+        KvGrid<1> *kvGrid1;
+        KvGrid<3> *kvGrid3;
+        KvGrid<4> *kvGrid4;
+        KvGrid<6> *kvGrid6;
+    };
 
-	struct {
-		Graphics::Wid::Text pageNum;
-	} widget;
+    struct PageTitle {
+        Graphics::Wid::Text textWidget;  
+    };
 
-	struct {
-	};
+    struct {
+        Graphics::Wid::Text pageNum;
+        PageTitle pageTitle; 
+    } widget;
 
 private:
-	void widgetPageNumFormatUpdate();
+    void widgetPageNumFormatUpdate();
+    void widgetPageTitleUpdate();
 
-	template <unsigned Ncols, class Tvec>
-	static void gridSetKv(KvGrid<Ncols> &aGrid, const char **aLabels, Tvec &&aValues)
-	{
-		for (unsigned i = 0; i < Ncols; ++i) {
-			const char *label = aLabels != nullptr ? aLabels[i] : nullptr;
+    template <unsigned Ncols, class Tvec>
+    static void gridSetKv(KvGrid<Ncols> &aGrid, const char **aLabels, Tvec &&aValues) {
+        for (unsigned i = 0; i < Ncols; ++i) {
+            const char *label = aLabels != nullptr ? aLabels[i] : nullptr;
+            aGrid.setValue(i, aValues[i]);
+            if (label != nullptr) {
+                aGrid.setKey(i, label);
+            }
+        }
+    }
 
-			aGrid.setValue(i, aValues[i]);
+    template <unsigned Ncols>
+    void gridInit(KvGrid<Ncols> **aGrid, const char *aKey, bool aUseNumSuffix) {
+        *aGrid = new KvGrid<Ncols>(ren.kvGrid, aKey, aUseNumSuffix, &gridOffset, &backgroundColor, &Graphics::Font::kDefault, &textColor, textSize);
+        (*aGrid)->setPriority(1);
+    }
 
-			if (label != nullptr) {
-				aGrid.setKey(i, label);
-			}
-		}
-	}
-
-	template <unsigned Ncols>
-	void gridInit(KvGrid<Ncols> **aGrid, const char *aKey, bool aUseNumSuffix)
-	{
-		*aGrid = new KvGrid<Ncols>(ren.kvGrid, aKey, aUseNumSuffix, &gridOffset, &backgroundColor, &Graphics::Font::kDefault, &textColor, textSize);
-		(*aGrid)->setPriority(1);
-	}
-
-	template <unsigned Ncols>
-	void gridDeinit(KvGrid<Ncols> **aGrid)
-	{
-		if (*aGrid != nullptr) {
-			delete *aGrid;
-			*aGrid = nullptr;
-		}
-	}
+    template <unsigned Ncols>
+    void gridDeinit(KvGrid<Ncols> **aGrid) {
+        if (*aGrid != nullptr) {
+            delete *aGrid;
+            *aGrid = nullptr;
+        }
+    }
 
 public:
-	Lcd();
-	void updateInfo(const Lcs::Info &);
-	void slide(short aShift);
-	void render();
+    Lcd();
+    void updateInfo(const Lcs::Info &);
+    void slide(short aShift);
+    void render();
 };
 
 }  // namespace App
--- Core/Src/App/Lcd.cpp
+++ Core/Src/App/Lcd.cpp
@@ -1,10 +1,3 @@
-//
-// Lcd.cpp
-//
-// Created on: Feb 07, 2022
-//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
-//
-
 #include "hagl.h"
 #include "Lcs/Types.hpp"
 #include "Lcs/Info.hpp"
@@ -19,150 +12,165 @@
 
 namespace App {
 
-void Lcd::widgetPageNumFormatUpdate()
-{
-	static constexpr const char *kFormat = " Page %d / %d ";
-	widget.pageNum.setProperty(Graphics::Text{kFormat, static_cast<int>(page) + 1, knPages});
+void Lcd::widgetPageNumFormatUpdate() {
+    static constexpr const char *kFormat = " Page %d / %d ";
+    widget.pageNum.setProperty(Graphics::Text{kFormat, static_cast<int>(page) + 1, knPages});
 }
 
-Lcd::Lcd():
-	page{Page::Main},
-	backgroundColor{hagl_color(255, 255, 255), hagl_color(255, 255, 255)},
-	fillDisplay{ren.background, &backgroundColor},
-	slider{ren.background},
-	textColor{hagl_color(0, 0, 0)},
-	textSize{{0, 0}, {40, 15}},
-	gridOffset{{5, 5}},
-	kvGrid6{nullptr},
-	widget{
-		{ren.background, &Graphics::Font::kDefault, Graphics::BgColor{hagl_color(255, 255, 255), hagl_color(0, 0, 0)}, Graphics::Area{{{5, 130}}, {{120, 150}}}}
-	}
-{
-	slide(0);
-	widget.pageNum.setPriority(2);
-	widgetPageNumFormatUpdate();
+void Lcd::widgetPageTitleUpdate() {
+    static const char *kPageTitles[] = {
+        "Main Page",    // Page::Main
+        "Active",       // Page::Active
+        "Enabled",      // Page::Enabled
+        "Amplitude",     // Page::Amplitude
+    };
+
+    widget.pageTitle.textWidget.setProperty(Graphics::Text{kPageTitles[static_cast<int>(page)], knPages}); 
 }
 
-void Lcd::updateInfo(const Lcs::Info &aInfo)
+Lcd::Lcd() :
+    page{Page::Main},
+    backgroundColor{hagl_color(255, 255, 255), hagl_color(255, 255, 255)},
+    fillDisplay{ren.background, &backgroundColor},
+    slider{ren.background},
+    textColor{hagl_color(0, 0, 0)},
+    textSize{{0, 0}, {40, 15}},
+    gridOffset{{5, 25}},
+    kvGrid6{nullptr},
+    widget{
+        {ren.background, &Graphics::Font::kDefault, Graphics::BgColor{hagl_color(255, 255, 255), hagl_color(0, 0, 0)}, Graphics::Area{{{15, 130}}, {{120, 150}}}},
+        {
+        {ren.background, &Graphics::Font::kDefault, Graphics::BgColor{hagl_color(0, 0, 0), hagl_color(255, 255, 255)}, Graphics::Area{{{30, 5}}, {{120, 25}}}} 
+        }  
+    }
 {
-	switch (page) {
-		case Page::Main: {  // Position
-			static const char *posLabels[] = {"x", "y", "z"};
-			auto &pos = aInfo.getPos();
-			for (int i = 0; i < 3; ++i) {
-				kvGrid6->setKey(i, posLabels[i]);
-				kvGrid6->setValue(i, pos[i]);
-			}
-
-    		static const char *eulerLabels[] = {"roll", "pitch", "yaw"};
-    		static const char *kFpFormat = "%3d.%.1d"; // Формат для float
-    		auto euler = aInfo.getEuler();
-    		for (int i = 0; i < 3; ++i) {
-        		char buffer[280]; 
-        		snprintf(buffer, sizeof(buffer), kFpFormat, euler[i] / 10, euler[i] % 10); 
-
-        		kvGrid6->setKey(i + 3, eulerLabels[i]);
-        		kvGrid6->setValue(i + 3, Graphics::Text{buffer}); // Используем Graphics::Text
-			}
-
-			break;
-		}
-
-		case Page::Active: {  // Active beacons
-			auto activeBeacons = aInfo.getActive();
-			gridSetKv(*kvGrid4, nullptr, activeBeacons);
-
-			break;
-		}
-
-		case Page::Enabled: {  // Enabled beacons
-			auto enabledBeacons = aInfo.getEnabled();
-			gridSetKv(*kvGrid4, nullptr, enabledBeacons);
-
-			break;
-		}
-
-		case Page::Amplitude: {
-			const auto &ampArr = aInfo.getAmpArr();
-			gridSetKv(*kvGrid4, nullptr, ampArr);
-
-			break;
-		}
-
-		default:
-			assert(false);  // The proper handler should be written
-	}
-
-	ren.update.kvGrid = true;
+    slide(0);
+    widget.pageNum.setPriority(2);
+    widget.pageTitle.textWidget.setPriority(2); 
+    widgetPageNumFormatUpdate();
+    widgetPageTitleUpdate();
 }
 
-void Lcd::slide(short aShift)
-{
-	// Deallocate grid
-	switch (page) {
-		case Page::Main:
-			gridDeinit(&kvGrid6);
+void Lcd::updateInfo(const Lcs::Info &aInfo) {
+    switch (page) {
+        case Page::Main: {  // Position
+            static const char *posLabels[] = {"x", "y", "z"};
+            auto &pos = aInfo.getPos();
+            for (int i = 0; i < 3; ++i) {
+                kvGrid6->setKey(i, posLabels[i]);
+                kvGrid6->setValue(i, pos[i]);
+            }
 
-			break;
+            static const char *eulerLabels[] = {"roll", "pitch", "yaw"};
+            static const char *kFpFormat = "%3d.%.1d";
+            auto euler = aInfo.getEuler();
+            for (int i = 0; i < 3; ++i) {
+                char buffer[280]; 
+                snprintf(buffer, sizeof(buffer), kFpFormat, euler[i] / 10, euler[i] % 10); 
 
-		case Page::Active:
-		case Page::Enabled:
-		case Page::Amplitude:
-			gridDeinit(&kvGrid4);
+                kvGrid6->setKey(i + 3, eulerLabels[i]);
+                kvGrid6->setValue(i + 3, Graphics::Text{buffer});
+            }
 
-			break;
+            break;
+        }
 
-		default:
-			assert(false);  // The proper handler should be written
-	}
+        case Page::Active: {  // Active beacons
+            auto activeBeacons = aInfo.getActive();
+            gridSetKv(*kvGrid4, nullptr, activeBeacons);
 
-	// Calculate new page index
-	aShift %= knPages;
-	auto fOverflow = (-aShift > static_cast<short>(page));
-	page = static_cast<Page>(fOverflow ? (knPages + aShift) : (static_cast<short>(page) + aShift) % knPages);
+            break;
+        }
 
-	// Initialize new page widget
-	switch (page) {
-		case Page::Main:
-			gridInit(&kvGrid6, "", false);
+        case Page::Enabled: {  // Enabled beacons
+            auto enabledBeacons = aInfo.getEnabled();
+            gridSetKv(*kvGrid4, nullptr, enabledBeacons);
 
-			break;
+            break;
+        }
 
-		case Page::Active:
-			gridInit(&kvGrid4, "act.", true);
+        case Page::Amplitude: {
+            const auto &ampArr = aInfo.getAmpArr();
+            gridSetKv(*kvGrid4, nullptr, ampArr);
 
-			break;
+            break;
+        }
 
-		case Page::Enabled:
-			gridInit(&kvGrid4, "en.", true);
+        default:
+            assert(false);  // The proper handler should be written
+    }
 
-			break;
-
-		case Page::Amplitude:
-			gridInit(&kvGrid4, "amp ", true);
-
-			break;
-
-		default:
-			assert(false);  // The proper handler should be written
-	}
-
-	widgetPageNumFormatUpdate();
-
-	ren.update.background = true;
+    ren.update.kvGrid = true;
 }
 
-void Lcd::render()
-{
-	if (ren.update.background) {
-		ren.background.run();
-		ren.update.background = false;
-	}
+void Lcd::slide(short aShift) {
+    // Deallocate grid
+    switch (page) {
+        case Page::Main:
+            gridDeinit(&kvGrid6);
 
-	if (ren.update.kvGrid) {
-		ren.kvGrid.run();
-		ren.update.kvGrid = false;
-	}
+            break;
+
+        case Page::Active:
+        case Page::Enabled:
+        case Page::Amplitude:
+            gridDeinit(&kvGrid4);
+
+            break;
+
+        default:
+            assert(false);  // The proper handler should be written
+    }
+
+    // Calculate new page index
+    aShift %= knPages;
+    auto fOverflow = (-aShift > static_cast<short>(page));
+    page = static_cast<Page>(fOverflow ? (knPages + aShift) : (static_cast<short>(page) + aShift) % knPages);
+
+    // Initialize new page widget
+    switch (page) {
+        case Page::Main:
+            gridInit(&kvGrid6, "", false);
+
+            break;
+
+        case Page::Active:
+            gridInit(&kvGrid4, "act.", true);
+
+            break;
+
+        case Page::Enabled:
+            gridInit(&kvGrid4, "en.", true);
+
+            break;
+
+        case Page::Amplitude:
+            gridInit(&kvGrid4, "amp ", true);
+
+            break;
+
+        default:
+            assert(false);  // The proper handler should be written
+    }
+
+    widgetPageNumFormatUpdate();
+    widgetPageTitleUpdate(); 
+
+    ren.update.background = true;
+}
+
+void Lcd::render() {
+    if (ren.update.background) {
+        ren.background.run();
+        ren.update.background = false;
+    }
+
+    if (ren.update.kvGrid) {
+        ren.kvGrid.run();
+        widget.pageTitle.textWidget.render(); 
+        widget.pageNum.render();  
+        ren.update.kvGrid = false;
+    }
 }
 
 }  // namespace App
